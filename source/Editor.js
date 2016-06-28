c368411a0b49622d3ca868ea5c0a9314836bf9e7
adrien <adrien@buildog.com>
2016-06-28 08:37:35 +0200

    fix scroll to top on formating

--- source/Editor.js
+++ source/Editor.js
@@ -1507,7 +1507,7 @@ proto.insertPlainText = function ( plainText, isPaste ) {
 var command = function ( method, arg, arg2 ) {
     return function () {
         this[ method ]( arg, arg2 );
-        return this.focus();
+        //return this.focus(); will lose scroll position
     };
 };
 
